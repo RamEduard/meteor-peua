a5911e0302ecc6d8f46d4775a308c88c3849941b
RamEduard <ramon.calle.88@gmail.com>
2016-12-11 22:05:28 -0400

    Dashboard posts publication updated.

--- server/collections/posts/dashboardPublications.js
+++ server/collections/posts/dashboardPublications.js
@@ -4,12 +4,10 @@ import { check, Match } from 'meteor/check';
 /**
  * Publish all events on dashboard
  */
-Meteor.publish("dashboard.posts", function (limit) {
-  check(limit, Number);
-
+Meteor.publish("dashboard.posts", function () {
   if (this.userId && Roles.userIsInRole(this.userId, ['admin'])) {
     return [
-      Posts.find({}, {limit: limit})
+      Posts.find()
     ];
   }
 
